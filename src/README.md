17debdcdb55ffd6f99af3a91fbb744f7ef8320b0
Kanan.Yusubov <kanan.yusubov@010999-MAC.local>
2025-12-29 09:43:26 +0400

    Readme file provided

---
+++ src/README.md
@@ -0,0 +1,28 @@
+# Algorithms and Data Structures with Kotlin
+
+---
+
+## Data structures implementations
+
+[SinglyLinkedList](src/main/dataStructuresAndAlgorithms/linkedList)
+
+---
+
+### LeetCode Solutions
+
+[1. Two Sum](src/main/leetcodeSolutions/TwoSum.kt)
+
+[9. Palindrome Number](src/main/leetcodeSolutions/PalindromeNumber.kt)
+
+[13. Roman to integer](src/main/leetcodeSolutions/RomanToInteger.kt)
+
+[14. Longest Common Prefix](src/main/leetcodeSolutions/LongestCommonPrefix.kt)
+
+[20. Valid Parentheses](src/main/leetcodeSolutions/ValidParentheses.kt)
+
+[21. Merge Two Sorted List](src/main/leetcodeSolutions/MergeTwoSortedList.kt)
+
+[26. Remove Duplicates From Sorted Array](src/main/leetcodeSolutions/RemoveDuplicatesFromSortedArray.kt)
+
+
+
---
+++ src/main/kotlin/leetcodeSolutions/RemoveDuplicatesFromSortedArray.kt
@@ -0,0 +1,29 @@
+package leetcodeSolutions
+
+/**
+ * 26. Remove Duplicates from Sorted Array
+ * link: https://leetcode.com/problems/remove-duplicates-from-sorted-array
+ */
+
+class Solution26 {
+    fun removeDuplicates(nums: IntArray): Int {
+        if (nums.isEmpty()) return 0
+
+        var uniqueIndex = 0
+        var lastUniqueValue = nums[uniqueIndex]
+        var uniqueCounter = 1
+
+        for (currentIndex in 1 until nums.size) {
+            val currentValue = nums[currentIndex]
+
+            if (currentValue != lastUniqueValue) {
+                uniqueIndex++
+                nums[uniqueIndex] = currentValue
+                lastUniqueValue = currentValue
+                uniqueCounter++
+            }
+        }
+
+        return uniqueCounter
+    }
+}
