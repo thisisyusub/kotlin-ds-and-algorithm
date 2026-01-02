c44ab304751a43993b7b2979cf95c5ef517350ec
thisisyusub <kanan.yusub@gmail.com>
2026-01-02 21:12:51 +0400

    Search Insert position problem solved

---
+++ src/main/kotlin/leetcodeSolutions/SearchInsertPosition.kt
@@ -0,0 +1,32 @@
+package leetcodeSolutions
+
+/**
+ * 35. Search Insert position
+ * link: https://leetcode.com/problems/search-insert-position
+ */
+
+class Solution35 {
+    fun searchInsert(nums: IntArray, target: Int): Int {
+        var startIndex = 0
+        var endIndex = nums.size - 1
+
+        while (startIndex <= endIndex) {
+            val mid = (startIndex + endIndex) / 2
+            val currentMidValue = nums[mid]
+
+            if (currentMidValue == target) {
+                return mid
+            }
+
+            if (target < currentMidValue) {
+                endIndex = mid - 1
+            }
+
+            if (target > currentMidValue) {
+                startIndex = mid + 1
+            }
+        }
+
+        return startIndex
+    }
+}
